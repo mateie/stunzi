2179d1c38ebf6144c897cb49ff2d4b743862ce14
Mateie <mateie@email.com>
2022-03-18 16:15:48 -0700

    added permission checking

--- src/classes/Command.ts
+++ src/classes/Command.ts
@@ -1,11 +1,14 @@
 import { roleMention, SlashCommandBuilder } from "@discordjs/builders";
+import { PermissionResolvable } from "discord.js";
 import Client from "./Client";
 
 export default class Command {
     client: Client;
     data: Omit<SlashCommandBuilder, "addSubcommand" | "addSubcommandGroup">;
+    permission: PermissionResolvable | undefined;
     constructor(client: Client) {
         this.client = client;
+        this.permission = undefined;
         this.data = new SlashCommandBuilder();
     }
 
