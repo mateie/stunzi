c01a9cad311ba8db1daf87542539c2ba769ff311
Mateie <mateie@email.com>
2022-03-18 15:48:17 -0700

    added permission to ICommand

--- src/classes/interfaces/ICommand.ts
+++ src/classes/interfaces/ICommand.ts
@@ -1,9 +1,10 @@
-import { CommandInteraction } from "discord.js";
+import { CommandInteraction, PermissionResolvable } from "discord.js";
 import Client from "../Client";
 import { SlashCommandBuilder, SlashCommandSubcommandsOnlyBuilder } from "@discordjs/builders";
 
 export default interface Command {
     client: Client,
+    permission: PermissionResolvable | undefined;
     data:
     | Omit<SlashCommandBuilder, "addSubcommandGroup" | "addSubcommand">
     | SlashCommandSubcommandsOnlyBuilder,
