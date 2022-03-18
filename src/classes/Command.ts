385c3f36329cc5861b161146beeb3ba727e28fc5
Mateie <mateie@email.com>
2022-03-18 16:48:56 -0700

    Updated command class

--- src/classes/Command.ts
+++ src/classes/Command.ts
@@ -1,18 +1,19 @@
-import { roleMention, SlashCommandBuilder } from "@discordjs/builders";
+import { roleMention, SlashCommandBuilder, SlashCommandSubcommandsOnlyBuilder } from "@discordjs/builders";
 import { PermissionResolvable } from "discord.js";
 import Client from "./Client";
 
 export default class Command {
     client: Client;
-    data: Omit<SlashCommandBuilder, "addSubcommand" | "addSubcommandGroup">;
+    data:
+        | SlashCommandBuilder;
     permission: PermissionResolvable | undefined;
     constructor(client: Client) {
         this.client = client;
         this.permission = undefined;
-        this.data = new SlashCommandBuilder();
+        this.data = <SlashCommandBuilder>new SlashCommandBuilder();
     }
 
-    mentionRole(roleId: string) {
+    mentionRole(roleId: string): string {
         return roleMention(roleId);
     }
 }
