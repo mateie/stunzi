72433583c20697c64620aea5defc180a6a688889
Mateie <mateie@email.com>
2022-03-18 18:10:50 -0700

    added all database manipulation to it's parent class

--- src/classes/Database.ts
+++ src/classes/Database.ts
@@ -2,11 +2,26 @@ const { DB } = process.env;
 import mongoose from "mongoose";
 import Client from "./Client";
 
+import Check from "./database/Check";
+import Verify from "./database/Verify";
+import Create from "./database/Create";
+import Get from "./database/Get";
+
 export default class Database {
     client: Client;
 
+    check: Check;
+    create: Create;
+    get: Get;
+    verify: Verify;
+
     constructor(client: Client) {
         this.client = client;
+
+        this.check = new Check(client, this);
+        this.create = new Create(client, this);
+        this.get = new Get(client, this);
+        this.verify = new Verify(client, this);
     }
 
     connect() {
