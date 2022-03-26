da2784785a45dadd71ca8c495477481098d8decb
Mateie <mateie@email.com>
2022-03-25 20:53:42 -0700

    Menu class

---
+++ src/classes/Menu.ts
@@ -0,0 +1,19 @@
+import { roleMention, ContextMenuCommandBuilder } from "@discordjs/builders";
+import { PermissionResolvable } from "discord.js";
+import Client from "./Client";
+
+export default class Menu {
+    client: Client;
+    data:
+        | ContextMenuCommandBuilder;
+    permission: PermissionResolvable | undefined;
+    constructor(client: Client) {
+        this.client = client;
+        this.permission = undefined;
+        this.data = <ContextMenuCommandBuilder>new ContextMenuCommandBuilder();
+    }
+
+    mentionRole(roleId: string): string {
+        return roleMention(roleId);
+    }
+}
