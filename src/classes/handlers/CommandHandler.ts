96a79f36876d220b203aae6894fcda83e71a5346
Mateie <mateie@email.com>
2022-03-27 05:24:18 -0700

    added reloading for handlers

--- src/classes/handlers/CommandHandler.ts
+++ src/classes/handlers/CommandHandler.ts
@@ -42,4 +42,33 @@ export default class CommandHandler {
 
         console.log(table.toString());
     }
+
+    async reload() {
+        const table = new Ascii('Commands Reloaded');
+        const files = await this.files;
+
+        this.client.commands.clear();
+
+        files.forEach(async file => {
+            const { default: Command } = require(file);
+            if (typeof Command !== 'function') return table.addRow('❌ Command is not a class');
+            const command = new Command(this.client);
+
+            if (!command.data) return table.addRow(file.split('/')[7], '❌ Failed', 'Missing data');
+            if (!command.data.name) return table.addRow(file.split('/')[7], '❌ Failed', 'Missing name');
+            if (!command.data.description) return table.addRow(command.data.name, '❌ Failed', 'Missing description');
+            if (command.permission) {
+                if (perms.includes(command.permission)) command.data.defaultPermission = false;
+                else return table.addRow(command.data.name, '❌ Failed', 'Permission is invalid');
+            }
+            if (!command.run) return table.addRow(command.data.name, '❌ Failed', 'Missing `run` function')
+            if (typeof command.run !== 'function') return table.addRow(command.data.name, '❌ Failed', '`run` should be a function');
+
+            this.client.commands.set(command.data.name, command);
+
+            await table.addRow(command.data.name, '✔ Reloaded');
+        });
+
+        console.log(table.toString());
+    }
 }
--- src/classes/handlers/MenuHandler.ts
+++ src/classes/handlers/MenuHandler.ts
@@ -42,4 +42,33 @@ export default class MenuHandler {
 
         console.log(table.toString());
     }
+
+    async reload() {
+        const table = new Ascii('Menus Reloaded');
+        const files = await this.files;
+
+        this.client.menus.clear();
+
+        files.forEach(async file => {
+            const { default: Menu } = require(file);
+            if (typeof Menu !== 'function') return table.addRow('❌ Menu is not a class');
+            const menu = new Menu(this.client);
+
+            if (!menu.data) return table.addRow(file.split('/')[7], '❌ Failed', 'Missing data');
+            if (!menu.data.name) return table.addRow(file.split('/')[7], '❌ Failed', 'Missing name');
+            if (!menu.data.type) return table.addRow(menu.data.name, '❌ Failed', 'Missing Type');
+            if (menu.permission) {
+                if (perms.includes(menu.permission)) menu.data.defaultPermission = false;
+                else return table.addRow(menu.data.name, '❌ Failed', 'Permission is invalid');
+            }
+            if (!menu.run) return table.addRow(menu.data.name, '❌ Failed', 'Missing `run` function');
+            if (typeof menu.run !== 'function') return table.addRow(menu.data.name, '❌ Failed', '`run` should be a function');
+
+            this.client.menus.set(menu.data.name, menu);
+
+            await table.addRow(menu.data.name, '✔ Reloaded');
+        });
+
+        console.log(table.toString());
+    }
 }
