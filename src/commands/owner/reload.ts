243ed2a4d51c6481c913f433f13820e53dd72bba
Mateie <mateie@email.com>
2022-03-27 05:24:25 -0700

    reload comman

---
+++ src/commands/owner/reload.ts
@@ -0,0 +1,24 @@
+import { CommandInteraction } from 'discord.js';
+import Client from '../../classes/Client';
+import Command from '../../classes/Command';
+import ICommand from '../../classes/interfaces/ICommand';
+
+export default class ReloadCommand extends Command implements ICommand {
+    constructor(client: Client) {
+        super(client);
+
+        this.permission = 'ADMINISTRATOR';
+        this.data
+            .setName('reload')
+            .setDescription('Reload most things on the bot');
+    }
+
+    async run(interaction: CommandInteraction) {
+        await this.client.commandHandler.reload();
+        await this.client.menuHandler.reload();
+        await this.client.eventHandler.reload();
+        await this.client.deploy();
+
+        interaction.reply({ content: 'Realoded', ephemeral: true });
+    }
+}
