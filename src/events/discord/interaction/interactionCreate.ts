a11878f07b7de18648c0854c7d5478bcfd07228a
Mateie <mateie@email.com>
2022-04-06 21:22:42 -0700

    error catching

--- src/events/discord/interaction/interactionCreate.ts
+++ src/events/discord/interaction/interactionCreate.ts
@@ -23,7 +23,7 @@ export default class InteractionCreate extends Event implements IEvent {
         const guild = <Guild>interaction.guild;
         const member = <GuildMember>interaction.member;
         const channel = <TextChannel>interaction.channel;
-        if (interaction.type == 'APPLICATION_COMMAND') {
+        if (interaction.type === 'APPLICATION_COMMAND') {
             const isBlocked = await this.client.blocks.isBlocked(member);
             if (isBlocked) {
                 const block = await this.client.blocks.get(member);
@@ -32,33 +32,25 @@ export default class InteractionCreate extends Event implements IEvent {
             }
         }
 
-        if (commandName !== 'music' && channel.id === channels.text.music) return interaction.reply({ content: `You can only use music commands here`, ephemeral: true })
+        if (interaction.type === 'APPLICATION_COMMAND' && commandName !== 'music' && channel.id === channels.text.music) return interaction.reply({ content: `You can only use music commands here`, ephemeral: true })
 
         if (interaction.isCommand()) {
             const command = <ICommand>this.client.commands.get(commandName);
-            if (!command) {
-                this.client.commands.delete(commandName);
-                return interaction.reply({ content: 'An error occurred', ephemeral: true });
-            }
 
             try {
-                command.run(interaction);
+                await command.run(interaction);
             } catch {
-                return interaction.reply({ content: 'An error occured, try again', ephemeral: true });
+                return interaction.reply({ content: 'An error occured, try again', ephemeral: true }).catch(console.error);
             }
         }
 
         if (interaction.isContextMenu()) {
             const menu = <IMenu>this.client.menus.get(commandName);
-            if (!menu) {
-                this.client.commands.delete(commandName);
-                return interaction.reply({ content: 'An error occured', ephemeral: true });
-            }
 
             try {
-                menu.run(interaction);
+                await menu.run(interaction);
             } catch {
-                return interaction.reply({ content: 'An error occured, try again', ephemeral: true });
+                return interaction.reply({ content: 'An error occured, try again', ephemeral: true }).catch(console.error);
             }
         }
     }
