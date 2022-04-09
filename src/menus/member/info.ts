71ef3b363ee06699d18f168cb325a6194c64e727
Mateie <mateie@email.com>
2022-04-09 01:33:50 -0700

    added function for member info

--- src/menus/member/info.ts
+++ src/menus/member/info.ts
@@ -18,43 +18,6 @@ export default class MemberMenu extends Menu implements IMenu {
 
         if (member.user.bot) return interaction.reply({ content: `${member} is a bot`, ephemeral: true });
 
-        const avatar = member.user.displayAvatarURL({ dynamic: true });
-        const activities: string[] = [];
-        const status = {
-            emoji: ':white_circle:',
-            text: 'Offline'
-        };
-
-        if (member.presence) {
-            member.presence.activities.forEach(act => {
-                const type = `***${act.type.charAt(0).toUpperCase() + act.type.split('_').join(' ').slice(1).toLowerCase()}***:`;
-
-                activities.push(`
-                    ${type} ${act.state ? this.client.util.list(act.state.split('; ')) : ''} ${act.type === 'PLAYING' ? act.name : ''} ${act.type === 'LISTENING' ? '-' : ''} ${act.details ? act.details : ''}
-                `);
-            });
-
-            status.emoji = this.client.util.statusEmoji(member.presence.status);
-            status.text = member.presence.status !== 'dnd' ? `${member.presence.status.charAt(0).toUpperCase()}${member.presence.status.slice(1)}` : 'Do Not Disturb';
-        }
-
-        const roles = member.roles.cache.filter(role => role.name !== '@everyone');
-        const mappedRoles = roles.map(role => this.mentionRole(role.id)).join(', ');
-
-        const embed = this.client.util.embed()
-            .setAuthor({ name: member.user.tag, iconURL: avatar, url: avatar })
-            .setColor(member.displayHexColor)
-            .setURL(avatar)
-            .setThumbnail(avatar)
-            .setDescription(`**Status**: ${status.emoji} ${status.text} ${activities.length > 0 ? `\n**Activities**: ${activities.join('')}` : ''}`)
-            .addFields([
-                { name: 'ID', value: member.id },
-                { name: 'Joined Server', value: `<t:${Math.floor(<number>member.joinedTimestamp / 1000)}:R>`, inline: true },
-                { name: 'Joined Discord', value: `<t:${Math.floor(<number>member.user.createdTimestamp / 1000)}:R>`, inline: true },
-                { name: `Roles (${roles.size})`, value: mappedRoles }
-            ]);
-
-        const rows = await this.client.util.memberActionRow(<GuildMember>interaction.member);
-        interaction.reply({ embeds: [embed], components: rows });
+        return this.client.util.memberInfo(interaction, member);
     }
 }
