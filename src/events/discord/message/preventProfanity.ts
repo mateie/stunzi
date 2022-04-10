3aa1bbed952006c07db3d1dbaccc64a48d644777
Mateie <mateie@email.com>
2022-04-09 22:51:01 -0700

    changed the name

---
+++ src/events/discord/message/preventProfanity.ts
@@ -0,0 +1,18 @@
+import Client from '@classes/Client';
+import Event from '@classes/Event';
+import IEvent from '@interfaces/IEvent';
+
+export default class PreventProfanityeEvent extends Event implements IEvent {
+    name: string;
+    once: undefined;
+
+    constructor(client: Client) {
+        super(client);
+
+        this.name = 'messageCreate';
+    }
+
+    async run() {
+        return;
+    }
+}
