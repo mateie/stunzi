89e741d467f30cd55e479a0302efa1b82e94dc41
Mateie <mateie@email.com>
2022-04-17 00:12:37 -0700

    changed up stuff

---
+++ src/types/index.d.ts
@@ -0,0 +1,6 @@
+declare module 'simple-discord-webhooks';
+declare module 'ascii-table';
+declare module 'valorant-api-js';
+declare module 'weather-js';
+declare module 'disrequire';
+declare module 'crafatar';
