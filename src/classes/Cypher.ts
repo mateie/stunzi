54ff0c95e01eb885a19c26b74189a99e4dcc2d21
Mateie <mateie@email.com>
2022-04-24 00:06:21 -0700

    added a method

--- src/classes/Cypher.ts
+++ src/classes/Cypher.ts
@@ -20,4 +20,14 @@ export default class Cypher {
     decrypt(str: string): string {
         return this.crypt.decrypt(str);
     }
+
+    makeSecret() {
+        let result = '';
+        const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        for (let i = 0; i < chars.length; i++) {
+            result += chars.charAt(Math.floor(Math.random() * chars.length));
+        }
+
+        return result;
+    }
 }
